d00ca167137ce8ee50fdea664de21155c95e7a3c
hsingh137 <himanshu.chauhanireland@gmail.com>
2024-05-16 10:23:04 +0100

    moves fiels

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -4,6 +4,25 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import { SampleForm } from 'jsonformbuilder';
 
+
+
+
+const FormSchema = {
+  "title": "Personal Information",
+  "type": "object",
+  "required": ["firstName", "lastName"],
+  "properties": {
+    "firstName": {
+      "type": "string",
+      "title": "First name"
+      
+    },
+    "lastName": {
+      "type": "string",
+      "title": "Last name"
+    }
+  }
+};
 function App() {
 
 
